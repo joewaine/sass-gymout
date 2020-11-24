00dedc68f8602d87fa406acf3073260eb2fb8ca7
alexagenesis1992 <alexagenesis1992@gmail.com>
2020-11-23 21:55:54 -0800

    f

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**Optionally add Bootstrap to your project**/
+import 'bootstrap';
+
+import '../css/style.scss';
+
+
+/********** Paste your code here! ************/
+
+console.log('Paste your code here!');
+
+function pauseVid() {
+    vid.pause();
+}
+
+let vid = document.getElementById("video");
+
+$('.play-video').click(function(){
+    vid.play();
+
+
+
+
+
+
+
+});
+
+
+$(".back-to-top").on("click", function() {
+    $("html, body").animate({ scrollTop: "0" }, 2000);
+});
+
+
+
