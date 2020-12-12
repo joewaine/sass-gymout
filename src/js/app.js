abc70dbb78e17c2ff41286e293125d5d683a50d1
alexagenesis1992 <alexagenesis1992@gmail.com>
2020-12-12 09:44:24 -0800

    stag

--- src/js/app.js
+++ src/js/app.js
@@ -43,3 +43,58 @@ $('.drop-down-menu').toggleClass('visible')
 });
 
 
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
+
+
+
+
+
+
+     $('.product-images img').click(function(e) {
+
+
+    $('.scroll-modal').addClass('visible')
+    
+    });
+    
+
+    $('.scroll-modal .close').click(function(e) {
+
+
+        $('.scroll-modal').removeClass('visible')
+        
+        });
+        
+
+
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            dots: false,
+            responsive:{
+              0:{
+                items:1
+            },
+              1000:{
+                    items:1
+                }
+            }
+        })
